1db00c629d96de002917f36625ab29bba7804a1d
01Jitlada <636051200047@mail.rmutk.ac.th>
2022-01-12 21:16:12 +0700

    ch1

--- app/src/main/java/com/example/minicalculator/MainActivity.kt
+++ app/src/main/java/com/example/minicalculator/MainActivity.kt
@@ -15,8 +15,7 @@ class MainActivity : AppCompatActivity() {
             total.setText("$sum")
             c1.setText("+")
         }
-
-        //wow
+        
         b2.setOnClickListener{
                 val n1 = a1.text.toString().toInt()
                 val n2 = a2.text.toString().toInt()
